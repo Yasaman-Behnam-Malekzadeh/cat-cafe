baec364438f3e401d2318369673d3eddba56b81e
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2025-05-18 12:50:28 +0200

    Change style of Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,20 @@ import forthCat from "../images/4.jpg";
 import fifthCat from "../images/5.jpg";
 import sixthCat from "../images/6.jpg";
 
+import bgWidth from "../images/bg-width.jpeg";
+
 function Home() {
   return (
     <div className="home" style={{ height: "100vh" }}>
-        <h1 className="d-flex justify-content-center align-items-end" style={{ height: "19%", fontSize:"82px"}}>Cat Cafe</h1>
+      <h1
+        className="d-flex justify-content-center align-items-end"
+        style={{ height: "15%", fontSize: "82px" }}
+      >
+        Cat Cafe
+      </h1>
       <div
         className="home d-flex justify-content-center align-items-end "
-        style={{ height: "80%", borderBottom: "2px solid" }}
+        style={{ height: "70%" }}
       >
         <img src={fisrtCat} alt="first cat" />
         <img src={secondCat} alt="secondCat" />
@@ -20,6 +27,14 @@ function Home() {
         <img src={fifthCat} alt="fifthCat" />
         <img src={sixthCat} alt="sixthCat" />
       </div>
+      <div
+        style={{
+          backgroundImage: `url(${bgWidth})`,
+          height: "30px",
+          backgroundSize: "contain",
+          backgroundRepeat: "repeat",
+        }}
+      ></div>
     </div>
   );
 }
